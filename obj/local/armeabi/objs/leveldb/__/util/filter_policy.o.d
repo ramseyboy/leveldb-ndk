ae759d76cd15a38c939b995619d29aa0ed3ff68c
ramseyboy <walker.hannan@willowtreeapps.com>
2014-07-14 20:33:33 -0400

    removed compiled code from repo

--- obj/local/armeabi/objs/leveldb/__/util/filter_policy.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi/objs/leveldb/__/./util/filter_policy.o: \
- jni/.././util/filter_policy.cc include/leveldb/filter_policy.h \
- /Users/walkerhannan/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/string
-
-include/leveldb/filter_policy.h:
-
-/Users/walkerhannan/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/string:
